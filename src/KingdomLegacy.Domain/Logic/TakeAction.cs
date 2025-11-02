a935a6ad177d07e64a6c6c5595cb4204ccf329cf
Mateusz Ledzianowski <mledzianowski@peakmobility.pl>
2025-11-02 20:58:36 +0100

    Bigger buttons.

--- src/KingdomLegacy.Domain/Logic/TakeAction.cs
+++ src/KingdomLegacy.Domain/Logic/TakeAction.cs
@@ -1,7 +1,7 @@
 ﻿namespace KingdomLegacy.Domain.Logic;
 internal class TakeAction(Game game, Card card) : ReversibleCardActionBase(game, card)
 {
-    public override State[] SourceStates => [State.Discovered, State.InPlay, State.Discarded, State.Removed];
+    public override State[] SourceStates => [State.Discovered, State.Discarded, State.Removed];
     public override State TargetState => State.Hand;
     public override bool Allowed => card.State != State.Removed || game._trash.Count > 0 && game._trash[0] == card;
     public override bool Disabled => false;
